5253a63869534030eb22d331373c8c7deff5942a
Woote <vivien.faure@wanadoo.fr>
2017-03-13 22:48:28 +0100

    no message

--- resources/scripts/common.js
+++ resources/scripts/common.js
@@ -9,7 +9,7 @@ function authSSO() {
 	let url = 'https://login.eveonline.com/oauth/authorize/';
 	let redirect_uri = 'https%3A%2F%2Fwoute.github.io%2Fcallback.html';
 	let scope = 'characterLocationRead%20characterNavigationWrite';
-	let state = (Math.random() * 10).toString();
+	let state = (Math.random() * 10).toString() + '_' + window.location.href;
 	url = url + '?response_type=code&redirect_uri=' + redirect_uri + '&client_id=' + clientID + '&scope=' + scope + '&state=' + state;
 	localStorage.setItem('state', state);
 	window.location.href = url;
