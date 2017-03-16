dc6414b3bd36e09a98105b546e1db7855a11e821
Woote <vivien.faure@wanadoo.fr>
2017-03-16 20:45:03 +0100

    no message

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -1,13 +1,5 @@
 'use strict';
 
-window.onload = function() {
-	document.body.onkeyup = function(e) {
-		if (e.keyCode == 32) {
-			goTo(localStorage.getItem('location'));
-		}
-	}
-}
-
 function authSSO() {
 	let url = 'https://login.eveonline.com/oauth/authorize/';
 	let redirect_uri = 'https%3A%2F%2Fwoute.github.io%2Fcallback.html';
