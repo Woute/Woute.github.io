b1d2561be417cd2598cedf19cdbf7f47caaaa53c
Woote <vivien.faure@wanadoo.fr>
2017-03-16 16:12:48 +0100

    no message

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -5,7 +5,15 @@ function authSSO() {
 	let redirect_uri = 'https%3A%2F%2Fwoute.github.io%2Fcallback.html';
 	let scope = 'characterLocationRead%20characterNavigationWrite';
 	let state = (Math.random() * 10).toString() + '_' + window.location.href;
+	let clientID = localStorage.getItem('clientID');
 	url = url + '?response_type=code&redirect_uri=' + redirect_uri + '&client_id=' + clientID + '&scope=' + scope + '&state=' + state;
 	localStorage.setItem('state', state);
 	window.location.href = url;
+}
+
+function readCredentials() {
+	let clientID = document.getElementById('clientID').value;
+	let secret = document.getElementById('secret').value;
+	localStorage.setItem('clientID', clientID);
+	localStorage.setItem('secret', secret);
 }
