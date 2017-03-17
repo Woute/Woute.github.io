f3979a1417abb12f540ee829bf4fd67073fca5f8
Woote <vivien.faure@wanadoo.fr>
2017-03-17 22:43:28 +0100

    no message

--- resources/scripts/system.js
+++ resources/scripts/system.js
@@ -66,7 +66,7 @@ function changeSystem() {
 	let system = tmp[1];
 	document.title = system;
 	document.getElementById('systemName').innerHTML = system;
-	history.pushState({}, system, window.location + '?location=' + region + '/' + system);
+	history.pushState({}, system, window.location.hostname + '/system.html?location=' + region + '/' + system);
 	checkResults(system);
 }
 
