83a790d36c2f92e735287b0648fe465c2ab9d15a
Woote <vivien.faure@wanadoo.fr>
2017-03-25 13:02:05 +0100

    no message

--- resources/scripts/common.js
+++ resources/scripts/common.js
@@ -56,7 +56,7 @@ function getLocation() {
 		})
 		.then(response => {
 			let result = JSON.parse(response);
-			location['region'] = result.name;
+			location['region'] = result.name.replace(' ', '_');
 			console.log('Current location : ' + location.region + '/' + location.name + '/' + location.id);
 			localStorage.setItem('location', JSON.stringify(location));
 			return resolve(location);
