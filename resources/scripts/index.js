ea3e516a256b38a1d5bb7f42d494b011208c76af
Woute <vivien.faure@wanadoo.fr>
2019-11-21 18:25:02 +0100

    Update index.js
    Changing scopes

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -25,7 +25,7 @@ document.onkeyup = function(e) {
 function authSSO() {
 	let url = 'https://login.eveonline.com/oauth/authorize/';
 	let redirect_uri = 'https%3A%2F%2Fwoute.github.io%2Fcallback.html';
-	let scope = 'characterLocationRead%20characterNavigationWrite';
+	let scope = 'esi-location.read_location.v1%20esi-location.read_ship_type.v1%20esi-bookmarks.read_character_bookmarks.v1%20esi-ui.write_waypoint.v1';
 	let state = (Math.random() * 10).toString() + '_' + window.location.href;
 	let clientID = localStorage.getItem('clientID');
 	url = url + '?response_type=code&redirect_uri=' + redirect_uri + '&client_id=' + clientID + '&scope=' + scope + '&state=' + state;
